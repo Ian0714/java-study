309aafb6edab1a2c53622dc80a64775a9a76f3ac
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-22 22:54:49 +0900

    1. Data Type Practice

---
+++ DataType.java
@@ -0,0 +1,12 @@
+public class DataType {
+    public static void main(String[] args){
+        System.out.println(6); //Number
+        System.out.println("Six"); // String
+        System.out.println(10+6); // 16
+        System.out.println("10"+"6"); // 106
+        System.out.println(6*6); // 36
+        //System.out.println("6"*"6"); is not allowed
+        System.out.println("1423".length()); // 4
+        //System.out.println(1423.length()); is not allowed
+    }
+}
