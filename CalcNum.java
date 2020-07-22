3f19f9512c06cbfadd894c955dc61b74040b86ab
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-22 23:01:27 +0900

    2. Number Calculation

---
+++ CalcNum.java
@@ -0,0 +1,11 @@
+public class CalcNum {
+    public static void main(String[] args){
+        System.out.println(12+2); // 14
+        System.out.println(6-4); // 2
+        System.out.println(10*2); // 20
+        System.out.println(8/2); // 4
+        System.out.println(Math.PI); // Pi
+        System.out.println((Math.floor(Math.PI))); // 3.0
+        System.out.println((Math.ceil(Math.PI))); // 4.0
+    }
+}
