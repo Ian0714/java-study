a27bf2d333dec1fc0ed1733a50ba31afe43f872e
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-22 23:08:08 +0900

    3. String

---
+++ String.java
@@ -0,0 +1,10 @@
+public class String {
+    public static void main(String[] args){
+        System.out.println("Hello"); // String
+        System.out.println('a'); // Character
+        System.out.println("Hello " 
+            + "World"); // Moving Line
+        System.out.println("Hello \nWorld"); // Moving Line
+        System.out.println("Hello \"World\""); // Hello "World"
+    }
+}
