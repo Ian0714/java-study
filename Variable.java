5339552d4378e76a44666b6ae60f5cf222db132b
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-23 15:04:32 +0900

    5, Variables

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        int a = 1; // Integer
+        System.out.println(a); // 1
+        double b = 3.3; // Real Number
+        System.out.println(b); // 3.3
+        String c = "Hello World"; // String
+        System.out.println(c); // Hello World
+        
+    }
+}
