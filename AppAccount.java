2e153bfa2f668409f00a3c753a5cb46d278926a6
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-24 14:53:14 +0900

    Making Variables

--- AppAccount.java
+++ AppAccount.java
@@ -1,12 +1,21 @@
 public class AppAccount {
     public static void main(String[] args) {
-        System.out.println("Value of Supply : "+ 1000.0);
-        System.out.println("Value of VAT : "+ (1000.0*0.1));
-        System.out.println("Total : "+ (1000.0+(1000.0*0.1)));
-        System.out.println("Expense : "+ (1000.0*0.3));
-        System.out.println("Income : "+ (1000.0-(1000.0*0.3)));
-        System.out.println("Dividend : "+ (1000.0-(1000.0*0.5)));
-        System.out.println("Dividend : "+ (1000.0-(1000.0*0.3)));
-        System.out.println("Dividend : "+ (1000.0-(1000.0*0.2)));
+        double sup = 1000.0;
+        double vat = sup * 0.1;
+        double total = sup + vat;
+        double expense = 0.3 * sup;
+        double income = sup - expense;
+        double divend1 = income * 0.5;
+        double divend2 = income * 0.3;
+        double divend3 = income * 0.2;
+
+        System.out.println("Value of Supply : "+ sup);
+        System.out.println("Value of VAT : "+ vat);
+        System.out.println("Total : "+ total);
+        System.out.println("Expense : "+ expense);
+        System.out.println("Income : "+ income);
+        System.out.println("Dividend : "+ divend1);
+        System.out.println("Dividend : "+ divend2);
+        System.out.println("Dividend : "+ divend3);
     }
 }
