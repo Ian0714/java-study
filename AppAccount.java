68be2a25e74e1ebaa74218288f3e8b1a3c79464a
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-24 14:59:49 +0900

    Input & Conditional Statements

--- AppAccount.java
+++ AppAccount.java
@@ -1,13 +1,28 @@
 public class AppAccount {
     public static void main(String[] args) {
-        double sup = 1000.0;
+        double sup = Double.parseDouble(args[0]);
         double vat = sup * 0.1;
         double total = sup + vat;
         double expense = 0.3 * sup;
         double income = sup - expense;
-        double divend1 = income * 0.5;
-        double divend2 = income * 0.3;
-        double divend3 = income * 0.2;
+
+        double divend1;
+        double divend2;
+        double divend3;
+
+        if(income >= 10000){
+            divend1 = income * 0.5;
+            divend2 = income * 0.3;
+            divend3 = income * 0.2;
+        } else if(income >= 5000) {
+            divend1 = income * 0.8;
+            divend2 = income * 0.1;
+            divend3 = income * 0.1;
+        } else {
+            divend1 = income * 1;
+            divend2 = income * 0;
+            divend3 = income * 0;
+        }
 
         System.out.println("Value of Supply : "+ sup);
         System.out.println("Value of VAT : "+ vat);
