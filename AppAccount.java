20b7cadea4d2e16eca0edb673c50e59472731f75
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-07-24 15:25:55 +0900

    Use Method

--- AppAccount.java
+++ AppAccount.java
@@ -1,10 +1,15 @@
 public class AppAccount {
+    public static double sup;
+    public static double vatrate;
+    public static double expenserate;
     public static void main(String[] args) {
-        double sup = Double.parseDouble(args[0]);
-        double vat = sup * 0.1;
-        double total = sup + vat;
-        double expense = 0.3 * sup;
-        double income = sup - expense;
+        sup = 10000.0;
+        vatrate = 0.1;
+        expenserate = 0.3;
+        double vat = VAT();
+        double total = TOTAL();
+        double expense = EXPENSE();
+        double income = INCOME();
 
         double[] divendRates = new double[3];
 
@@ -18,16 +23,36 @@ public class AppAccount {
             divendRates[2] = income * 0;
         }
 
-        System.out.println("Value of Supply : "+ sup);
-        System.out.println("Value of VAT : "+ vat);
-        System.out.println("Total : "+ total);
-        System.out.println("Expense : "+ expense);
-        System.out.println("Income : "+ income);
+        print();
 
         int i = 0;
         while(i < divendRates.length){
             System.out.println("Dividend "+ i + " : "+ (income*divendRates[i]));
             i += 1;
         }
+    }
+
+    public static void print(){
+        System.out.println("Value of Supply : "+ sup);
+        System.out.println("Value of VAT : "+ VAT());
+        System.out.println("Total : "+ TOTAL());
+        System.out.println("Expense : "+ EXPENSE());
+        System.out.println("Income : "+ INCOME());
+    }
+
+    public static double INCOME() {
+        return sup - EXPENSE();
+    }
+
+    public static double EXPENSE() {
+        return sup * expenserate;
+    }
+
+    public static double TOTAL() {
+        return sup + VAT();
+    }
+
+    public static double VAT() {
+        return sup * vatrate;
     }
 }
